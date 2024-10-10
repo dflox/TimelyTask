ef5597e296d98aad5ce9d5f6bae7ba3af0339a3b
dflox <f.dritter@htwg-konstanz.de>
2024-10-10 17:35:10 +0200

    Datatypes Optimized
    Created Deadline, Optimized Task and other classes.

---
+++ src/main/scala/Deadline.scala
@@ -0,0 +1,7 @@
+import com.github.nscala_time.time.Imports.DateTime
+
+case class Deadline(date: DateTime, initialDate: Option[DateTime], completionDate: Option[DateTime]) {
+  def this(date: DateTime) = this(date, None, None)
+  def this(date: DateTime, initialDate: DateTime) = this(date, Some(initialDate), None)
+  def this(date: DateTime, initialDate: DateTime, completionDate: DateTime) = this(date, Some(initialDate), Some(completionDate))
+}
--- src/main/scala/Task.scala
+++ src/main/scala/Task.scala
@@ -1,2 +1,14 @@
-case class Task (val id: Int, val name: String, val description: String, val prio: Priority, val status: Status) {
+import com.github.nscala_time.time.Imports._
+import java.util.UUID
+import scala.collection.immutable.HashSet
+
+case class Task (name: String, description: String,
+                 priority: Priority, deadline: Deadline, estimatedDuration: Period,
+                 dependentOn: HashSet[Task] = new HashSet[Task](), reoccurring: Boolean, recurrenceInterval: Period,
+                 status: Status) {
+  val uuid: UUID = UUID.randomUUID()
+  val realDuration: Option[Period] = None
+  val completionDate: Option[DateTime] = None
+
+
 }
