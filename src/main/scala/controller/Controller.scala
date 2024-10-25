df92cd98f4c72ba20af518f93a25ab46f1724a76
dflox <f.dritter@htwg-konstanz.de>
2024-10-25 09:39:21 +0200

    Added test creation of Objects

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -9,41 +9,31 @@ import java.awt.Color
 import java.util.UUID
 import scala.collection.immutable.HashSet
 import scala.collection.mutable
-import util.FileLoader
 import io.circe.generic.auto.*
 import model.settings.StartView.TABLE
 import model.settings.Theme.DARK
+import model.settings.{FileType, StartView, Theme, DataType}
 
 class Controller(view: View[List[Task]]) {
 
+  val tag: HashSet[UUID] = HashSet(new Tag(name = "test", description = "test").uuid)
+  val testTask1: Task = new Task("Test", "Test", new Priority(name = "test", description = "test", rank = 1, color = Color.BLACK, daysPreDeadline = 4, postponable = false).uuid, tag, new Deadline(date = DateTime.now()), new State(name = "test", description = "test", color = Color.BLACK).uuid, Period.days(1), new HashSet[UUID](), false, Period.days(1))
+  val testTask2: Task = new Task(name = "Test", description = "test", priority = new Priority(name = "test", description = "test", rank = 1, color = Color.BLACK, daysPreDeadline = 4, postponable = false).uuid, deadline =  new Deadline(date = DateTime.now()), state = new State(name = "test", description = "test", color = Color.BLACK).uuid, estimatedDuration = Period.days(1), reoccurring = false, recurrenceInterval = Period.days(1))
   var tasks: List[Task] = List()
   var tags: mutable.HashMap[UUID, Tag] = mutable.HashMap()
   var priorities: mutable.HashMap[UUID, Priority] = mutable.HashMap()
   var states: mutable.HashMap[UUID, State] = mutable.HashMap()
   val config: Config = Controller.getConfig
 
-  def loadConfigTest(): Unit = {
-    println("Running")
-    println("Config: " + config)
-    FileLoader.save[Config](JSON, "./", List(config))
-    println("Saved")
-    println("Loading")
-    val loadedConfig = FileLoader.load[Config](JSON, "./").getOrElse(List(new Config(TABLE, defaultDataFileType = XML, DARK))).head
-    println("Loaded: " + loadedConfig)
-  }
-
   def run(): Unit = {
-    loadConfigTest()
+    println(view.update(tasks));
   }
 }
 
 object Controller {
   val defaultConfigFolderPath: String = "./"
-
-  private def getConfig: Config = {
-    try {
-      FileLoader.load[Config](JSON, defaultConfigFolderPath).getOrElse(List(Config.defaultConfig)).head
-    } catch
-      case e: Exception => Config.defaultConfig
+  def getConfig: Config = {
+    val config: Config = new Config(defaultStartView = TABLE, defaultDataFileType = FileType.JSON, defaultTheme = Theme.DARK)
+    config
   }
 }
--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -1,7 +1,6 @@
 package model
 
 import com.github.nscala_time.time.Imports.*
-import model.fileio.*
 import model.settings.DataType
 
 import java.util.UUID
@@ -9,25 +8,10 @@ import scala.collection.immutable.HashSet
 
 case class Task(name: String, description: String,
                 priority: UUID, tags: HashSet[UUID] = new HashSet[UUID](), deadline: Deadline,
-                status: UUID, estimatedDuration: Period, dependentOn: HashSet[UUID] = new HashSet[UUID](),
-                reoccurring: Boolean, recurrenceInterval: Period)
-  extends TaskEncoders {
+                state: UUID, estimatedDuration: Period, dependentOn: HashSet[UUID] = new HashSet[UUID](),
+                reoccurring: Boolean, recurrenceInterval: Period) {
 
   val uuid: UUID = UUID.randomUUID()
   val realDuration: Option[Period] = None
   val completionDate: Option[DateTime] = None
-}
-
-object Task extends TaskEncoders {
-  implicit val yamlSerializable: YamlSerializable[Task] = new YamlSerializable[Task] {
-    override val YamlEncoder: Task => String = encodeToYaml
-    override val YamlDecoder: String => Task = decodeFromYaml
-  }
-  
-  implicit val xmlSerializable: XmlSerializable[Task] = new XmlSerializable[Task] {
-    override val dataType: DataType = DataType.TASK
-    override val XmlEncoder: Task => String = encodeToXml
-    override val XmlDecoder: String => Task = decodeFromXml
-  }
-    
 }
