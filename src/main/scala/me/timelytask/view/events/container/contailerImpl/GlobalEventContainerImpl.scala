ccad4a01d534676bf847b74353a401b86f21c6e1
dflox <30594936+dflox@users.noreply.github.com>
2025-06-29 14:38:16 +0200

    dev add model import functionality to CalendarViewGuiFactory and update getFileName method

--- src/main/scala/me/timelytask/view/events/container/contailerImpl/GlobalEventContainerImpl.scala
+++ src/main/scala/me/timelytask/view/events/container/contailerImpl/GlobalEventContainerImpl.scala
@@ -2,7 +2,7 @@ package me.timelytask.view.events.container.contailerImpl
 
 import me.timelytask.core.CoreModule
 import me.timelytask.model.settings.UIType.GUI
-import me.timelytask.model.settings.{TABLE, TASKEdit, ViewType}
+import me.timelytask.model.settings.{ TABLE, TASKEdit, ViewType }
 import me.timelytask.model.task.Task
 import me.timelytask.util.Publisher
 import me.timelytask.view.UiInstance
@@ -14,35 +14,40 @@ import org.joda.time.DateTime
 import scala.util.Random
 
 // TODO: add GlobalEventResolver and extend the Keymap to include the global events
-class GlobalEventContainerImpl(coreModule: CoreModule,
-                               eventHandler: EventHandler,
-                               activeViewPublisher: Publisher[ViewType],
-                               uiInstance: UiInstance,
-                               override val userToken: String
-                              ) extends GlobalEventContainer {
+class GlobalEventContainerImpl(
+    coreModule: CoreModule,
+    eventHandler: EventHandler,
+    activeViewPublisher: Publisher[ViewType],
+    uiInstance: UiInstance,
+    override val userToken: String)
+    extends GlobalEventContainer {
 
-  override def undo(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.commandHandler.undo()
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ) {})
+  override def undo(): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.commandHandler.undo()
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
 
-  override def redo(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.commandHandler.redo()
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ) {})
+  override def redo(): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.commandHandler.redo()
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
 
   override def switchToView(): Unit = {
     val event = new Event[Unit](
       _ => {
-        if(activeViewPublisher.getValue.contains(TASKEdit)) false
+        if (activeViewPublisher.getValue.contains(TASKEdit)) false
         else {
           activeViewPublisher.update(Some(TABLE))
           true
@@ -50,91 +55,108 @@ class GlobalEventContainerImpl(coreModule: CoreModule,
       },
       _ => None,
       ()
-    ){}
+    ) {}
     eventHandler.handle(event)
   }
 
-  override def shutdownApplication(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.coreController.shutdownApplication()
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ){})
-
-  override def newWindow(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      uiInstance.addUi(GUI)
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ){})
-
-  override def newInstance(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.coreController.newGuiInstance()
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ){})
-
-  override def addRandomTask(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.modelController.addTask(userToken, randomTask())
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ){})
-
-  override def exportModel(serializationType: String,
-                           folderPath: Option[String] = None,
-                           fileName: Option[String] = None): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.persistenceController.saveModel(userToken,
-        folderPath, fileName, serializationType)
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ) {})
-    
-  
-  
-  private def randomTask(): Task = {
-    new Task(
-      "Random Task",
-      "Random Task Description",
-      scheduleDate = DateTime.now()
-        .withTime(8, 0, 0, 0)
-        .plusMinutes(Random.nextInt(60*10))// random time today
-    )
-  }
-
-  override def newTask(): Unit = ()  //TODO
-  
-  // TODO: Update Task, change settings, change theme, change view (?) etc.
-
-  override def closeInstance(): Unit = eventHandler.handle(new Event[Unit](
-    (args: Unit) => {
-      coreModule.controllers.coreController.closeInstance(uiInstance)
-      true
-    },
-    (args: Unit) => None,
-    ()
-  ){})
-
-
-  override def importModel(serializationType: String, folderPathWithFileName: String): Unit = {
-    eventHandler.handle(new Event[Unit](
+  override def shutdownApplication(): Unit = eventHandler.handle(
+    new Event[Unit](
       (args: Unit) => {
-        coreModule.controllers.persistenceController.loadModel(userToken, folderPathWithFileName, serializationType)
+        coreModule.controllers.coreController.shutdownApplication()
         true
       },
       (args: Unit) => None,
       ()
-    ) {})
+    ) {}
+  )
+
+  override def newWindow(): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        uiInstance.addUi(GUI)
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
+
+  override def newInstance(): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.coreController.newGuiInstance()
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
+
+  override def addRandomTask(): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.modelController.addTask(userToken, randomTask())
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
+
+  override def exportModel(
+      serializationType: String,
+      folderPath: Option[String] = None,
+      fileName: Option[String] = None
+    ): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.persistenceController
+          .saveModel(userToken, folderPath, fileName, serializationType)
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
+
+  private def randomTask(): Task = {
+    new Task(
+      "Random Task",
+      "Random Task Description",
+      scheduleDate = DateTime
+        .now()
+        .withTime(8, 0, 0, 0)
+        .plusMinutes(Random.nextInt(60 * 10)) // random time today
+    )
+  }
+
+  override def newTask(): Unit = () // TODO
+
+  // TODO: Update Task, change settings, change theme, change view (?) etc.
+
+  override def closeInstance(): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.coreController.closeInstance(uiInstance)
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
+
+  override def importModel(
+      serializationType: String,
+      folderPathWithFileName: String
+    ): Unit = eventHandler.handle(
+    new Event[Unit](
+      (args: Unit) => {
+        coreModule.controllers.persistenceController
+          .loadModel(userToken, folderPathWithFileName, serializationType)
+        true
+      },
+      (args: Unit) => None,
+      ()
+    ) {}
+  )
 }
